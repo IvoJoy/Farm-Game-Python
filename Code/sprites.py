3580e69c64f948c7787a8bef0c71e21d586b64e2
IvoJoy <ivkata1112@abv.bg>
2025-01-29 00:53:19 +0200

    Adding Particles

--- Code/sprites.py
+++ Code/sprites.py
@@ -33,6 +33,23 @@ class Wildflower(Generic):
         super().__init__(pos, surf, groups)
         self.hitbox = self.rect.copy().inflate(-20, -self.rect.height * 0.9)
 
+class Particle(Generic):
+    def __init__(self, pos, surf, groups, z, duration = 200):
+        super().__init__(pos, surf, groups, z)
+        self.start_time = pygame.time.get_ticks()
+        self.duration = duration
+
+        mask_surf = pygame.mask.from_surface(self.image)
+        new_surf = mask_surf.to_surface()
+        new_surf.set_colorkey((0,0,0))
+        self.image = new_surf
+
+    def update(self, dt):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.start_time >= self.duration:
+            self.kill()
+
+
 class Tree(Generic):
     def __init__(self, pos, surf, groups, name):
         super().__init__(pos, surf, groups)
@@ -57,10 +74,12 @@ class Tree(Generic):
         # remove an apple
         if len(self.apple_sprites.sprites()) > 0:
             random_apple = choice(self.apple_sprites.sprites())
+            Particle(random_apple.rect.topleft, random_apple.image, self.all_sprites, LAYERS['fruit'])
             random_apple.kill()
 
     def check_death(self):
         if self.health <= 0:
+           Particle(self.rect.topleft, self.image, self.all_sprites, LAYERS['fruit'], 500)
            self.image = self.stump_surf
            self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
            self.hitbox = self.rect.copy().inflate(-10, -self.rect.height * 0.6)
