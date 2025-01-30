bf5868c3de94c17ec42d92119e6a7cad36f7f327
IvoJoy <ivkata1112@abv.bg>
2025-01-30 16:59:00 +0200

    Added raining

--- Code/level.py
+++ Code/level.py
@@ -8,6 +8,8 @@ import os
 from pytmx.util_pygame import load_pygame
 from support import *
 from soil import SoilLayer
+from sky import Rain
+from random import randint
 
 class Level:
     def __init__(self):
@@ -25,6 +27,11 @@ class Level:
         self.setup()
         self.overlay = Overlay(self.player)
         self.transition = Transition(self.reset, self.player)
+
+        # sky
+        self.rain = Rain(self.all_sprites)
+        self.raining = randint(0,10) > 3
+        self.soil_layer.raining = self.raining
         
 
     def setup(self):
@@ -86,6 +93,11 @@ class Level:
         # soil
         self.soil_layer.remove_water()
 
+        self.raining = randint(0,10) > 3
+        self.soil_layer.raining = self.raining
+        if self.raining:
+            self.soil_layer.water_all()
+
         # apples on trees
         for tree in self.tree_sprites.sprites():
             for apple in tree.apple_sprites.sprites():
@@ -99,6 +111,10 @@ class Level:
         self.all_sprites.update(dt)
 
         self.overlay.display()
+
+        if self.raining:
+            self.rain.update()
+
 
         if self.player.sleep:
             self.transition.play()
