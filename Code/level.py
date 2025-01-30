93320dd6adcc948ef2f898b7c658b9fe5f22e8ac
IvoJoy <ivkata1112@abv.bg>
2025-01-30 17:47:52 +0200

    Added daytime cycle

--- Code/level.py
+++ Code/level.py
@@ -8,7 +8,7 @@ import os
 from pytmx.util_pygame import load_pygame
 from support import *
 from soil import SoilLayer
-from sky import Rain
+from sky import Rain, Sky
 from random import randint
 
 class Level:
@@ -32,6 +32,7 @@ class Level:
         self.rain = Rain(self.all_sprites)
         self.raining = randint(0,10) > 3
         self.soil_layer.raining = self.raining
+        self.sky = Sky()
         
 
     def setup(self):
@@ -107,6 +108,9 @@ class Level:
                 apple.kill()
             tree.create_fruit()
 
+        # sky
+        self.sky.start_color = [255, 255, 255]
+
     def plant_collision(self):
         if self.soil_layer.plant_sprites:
             for plant in self.soil_layer.plant_sprites.sprites():
@@ -127,6 +131,7 @@ class Level:
         if self.raining:
             self.rain.update()
 
+        self.sky.display(dt)
 
         if self.player.sleep:
             self.transition.play()
